0a810b87d38f7bd3f14cfa81dc7a45d29967b66e
Maad <39955093+castielwallker@users.noreply.github.com>
2024-10-20 08:53:37 -0300

    NSFW.js

--- nsfw.js
+++ nsfw.js
@@ -1,56 +1,46 @@
 // ==UserScript==
 // @name         Erome NSFW Premium
-// @namespace    http://tampermonkey.net/
-// @version      2024-10-19
+// @namespace    https://github.com/castielwallker/
+// @version      1.0
 // @description  Aplica efeito de desfoque nas imagens de álbuns NSFW
-// @author       Você
+// @author       Maad
 // @match        https://www.erome.com/explore*
 // @match        https://www.erome.com/*
 // @match        https://www.erome.com/search?q=*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
 // ==/UserScript==
 
 (function () {
     'use strict';
-
-    // Adiciona estilo CSS para o efeito de desfoque
     const style = document.createElement('style');
     style.textContent = `
         .blur {
-            filter: blur(15px); /* Aplica o efeito de desfoque */
+            filter: blur(15px); 
         }
     `;
     document.head.appendChild(style);
-
-    const addButtonToNavbar = function () {
+    
+        const addButtonToNavbar = function () {
         const navbarRight = document.querySelector('.navbar-nav.navbar-right');
-
-        // Verifica se o botão já existe para não duplicar
         if (!document.getElementById('nsfw-toggle-btn')) {
             // Botão NSFW
             const nsfwButton = document.createElement('li');
             nsfwButton.innerHTML = '<a href="#" id="nsfw-toggle-btn">NSFW</a>';
             nsfwButton.querySelector('a').addEventListener('click', (e) => {
                 e.preventDefault();
-                const albumContainers = document.querySelectorAll('.album-thumbnail-container'); // Obtém todos os contêineres de miniaturas
-                const isBlurred = albumContainers[0].classList.toggle('blur'); // Verifica o estado de desfoque
-
+                  const albumContainers = document.querySelectorAll(
+                      '.album-thumbnail-container, .media-group img, .media-group video');
+                const isBlurred = albumContainers[0].classList.toggle('blur'); 
                 albumContainers.forEach(albumContainer => {
                     if (isBlurred) {
-                        albumContainer.classList.add('blur'); // Aplica o efeito de desfoque
+                        albumContainer.classList.add('blur');
                     } else {
-                        albumContainer.classList.remove('blur'); // Remove o efeito de desfoque
+                        albumContainer.classList.remove('blur');
                     }
                 });
             });
             navbarRight.appendChild(nsfwButton);
         }
     };
-
-    // Chama a função apenas se a URL for correspondente
-    if (window.location.href.match(/https:\/\/www\.erome\.com\/(explore.*|search\?q=.*|$)/)) {
-        addButtonToNavbar();
-    }
+ addButtonToNavbar();
 })();
 
