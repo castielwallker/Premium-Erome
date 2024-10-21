ed67444d505c38e193cdd2156f1ef3b8d91e56a3
Maad <39955093+castielwallker@users.noreply.github.com>
2024-10-21 08:53:24 -0300

    nsfw.js

--- nsfw.js
+++ nsfw.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Erome NSFW Premium
 // @namespace    https://github.com/castielwallker/
-// @version      1.2
+// @version      1.0
 // @description  Aplica efeito de desfoque nas imagens de álbuns NSFW
 // @author       Maad
 // @match        https://www.erome.com/explore*
@@ -11,53 +11,35 @@
 
 (function () {
     'use strict';
-
-    // Adiciona estilo para o efeito de blur e o pseudo-elemento
     const style = document.createElement('style');
     style.textContent = `
-        .blur::before {
-            content: '';
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: inherit;
+        .blur {
             filter: blur(15px);
-            z-index: 1;
-        }
-        .vjs-poster {
-            position: relative;
-            overflow: hidden; /* Garante que o pseudo-elemento não vaze */
         }
     `;
     document.head.appendChild(style);
 
-    // Função para adicionar o botão NSFW na barra de navegação
-    const addButtonToNavbar = function () {
+        const addButtonToNavbar = function () {
         const navbarRight = document.querySelector('.navbar-nav.navbar-right');
         if (!document.getElementById('nsfw-toggle-btn')) {
+            // Botão NSFW
             const nsfwButton = document.createElement('li');
             nsfwButton.innerHTML = '<a href="#" id="nsfw-toggle-btn">NSFW</a>';
             nsfwButton.querySelector('a').addEventListener('click', (e) => {
                 e.preventDefault();
-                // Seleciona os elementos que precisam de blur
-                const albumContainers = document.querySelectorAll(
-                    '.album-thumbnail-container, .media-group img, .media-group video, .videojs, .media-group,'
-                );
-                const isBlurred = albumContainers[0]?.classList.contains('blur');
+                  const albumContainers = document.querySelectorAll(
+                      '.album-thumbnail-container, .media-group img, .media-group video, .vjs-poster');
+                const isBlurred = albumContainers[0].classList.toggle('blur');
                 albumContainers.forEach(albumContainer => {
                     if (isBlurred) {
-                        albumContainer.classList.remove('blur');
-                    } else {
                         albumContainer.classList.add('blur');
+                    } else {
+                        albumContainer.classList.remove('blur');
                     }
                 });
             });
             navbarRight.appendChild(nsfwButton);
         }
     };
-
-    // Adiciona o botão assim que o conteúdo estiver carregado
-    document.addEventListener('DOMContentLoaded', addButtonToNavbar);
+ addButtonToNavbar();
 })();
