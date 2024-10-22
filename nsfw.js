15265dfea4764e35375370f7961a066e954b502e
Maad <39955093+castielwallker@users.noreply.github.com>
2024-10-22 10:14:22 -0300

    Update nsfw.js

--- nsfw.js
+++ nsfw.js
@@ -16,6 +16,36 @@
         }
     `;
     document.head.appendChild(style);
+    
+  // Toast Mensagem
+        function showToast(message, isError = false) {
+        const existingToasts = document.querySelectorAll('.toast');
+    
+        const toast = document.createElement('div');
+        toast.className = 'toast';
+        toast.textContent = message;
+        toast.style.cssText = `
+            position: fixed;
+            bottom: ${existingToasts.length > 0 ? (existingToasts.length * 60 + 20) + 'px' : '20px'};
+            right: 20px;
+            background-color: ${isError ? '#101010' : '#ffffff'};
+            color: ${isError ? '#ffffff' : '#101010'};
+            padding: 10px 20px;
+            border-radius: 10px;
+            font-size: 14px;
+            z-index: 9999;
+            box-shadow: 0 0 10px ${isError ? '#101010' : '#ffffff'}, 0 0 20px ${isError ? '#ffffff' : '#101010'};
+            transition: opacity 0.5s;
+            opacity: 1;
+        `;
+    
+        document.body.appendChild(toast);
+    
+        setTimeout(() => {
+            toast.style.opacity = 0;
+            setTimeout(() => toast.remove(), 100);
+        }, 500);
+    }
 
         const addButtonToNavbar = function () {
         const navbarRight = document.querySelector('.navbar-nav.navbar-right');
@@ -31,8 +61,10 @@
                 albumContainers.forEach(albumContainer => {
                     if (isBlurred) {
                         albumContainer.classList.add('blur');
+                        showToast('O blur foi ativado,Foi detectado ambiente adulto.');
                     } else {
                         albumContainer.classList.remove('blur');
+                        showToast('O blur foi desativado.');
                     }
                 });
             });
