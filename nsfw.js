65f3cbb446900f17b5813b237939f7cce9f2211f
Maad <39955093+castielwallker@users.noreply.github.com>
2024-10-24 15:32:48 -0300

    Update nsfw.js

--- nsfw.js
+++ nsfw.js
@@ -17,68 +17,25 @@
     document.head.appendChild(fontAwesome);
 
     const style = document.createElement('style');
-    style.textContent = `
+    style.textContent = 
         .blur {
             filter: blur(15px);
         }
         .fa-eye {
             margin-right: 8px;
         }
-        .toast {
-            position: fixed;
-            right: 20px;
-            background-color: #ffffff;
-            color: #101010;
-            padding: 10px 20px;
-            border-radius: 10px;
-            font-size: 14px;
-            z-index: 9999;
-            box-shadow: 0 0 10px #ffffff, 0 0 20px #101010;
-            transition: opacity 0.5s;
-            opacity: 1;
-        }
-    `;
+    ;
     document.head.appendChild(style);
-
-    // Variável global para armazenar referência ao Toast
-    let toastTimeout;
-    let isToastVisible = false;
-
-    function showToast(message, isError = false) {
-        if (isToastVisible) return;  // Impedir exibição de múltiplas toasts simultâneas
-
-        isToastVisible = true;  // Marcar Toast como visível
-
-        const toast = document.createElement('div');
-        toast.className = 'toast';
-        toast.textContent = message;
-
-        if (isError) {
-            toast.style.backgroundColor = '#101010';
-            toast.style.color = '#ffffff';
-        }
-
-        document.body.appendChild(toast);
-
-        toastTimeout = setTimeout(() => {
-            toast.style.opacity = 0;
-            setTimeout(() => {
-                toast.remove();
-                isToastVisible = false;  // Marcar Toast como não visível
-            }, 500);
-        }, 2000);
-    }
 
     const addButtonToNavbar = function () {
         const navbarRight = document.querySelector('.navbar-nav.navbar-right');
         if (!document.getElementById('nsfw-toggle-btn')) {
             const nsfwButton = document.createElement('li');
-            nsfwButton.innerHTML = `
+            nsfwButton.innerHTML = 
                 <a href="#" id="nsfw-toggle-btn" style="display: flex; align-items: center;">
                     <i class="fas fa-eye"></i> NSFW
                 </a>
-            `;
-
+            ;
             nsfwButton.querySelector('a').addEventListener('click', (e) => {
                 e.preventDefault();
 
@@ -91,10 +48,6 @@
                     albumContainer.classList.toggle('blur', isBlurred);
                 });
 
-                const message = isBlurred
-                    ? 'O blur foi ativado. Foi detectado ambiente adulto.'
-                    : 'O blur foi desativado.';
-                showToast(message);
             });
 
             navbarRight.appendChild(nsfwButton);
