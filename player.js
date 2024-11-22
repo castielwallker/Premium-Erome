a65f2aefb8065b3f06bfaec22b5e1fba2fbb440d
Maad <39955093+castielwallker@users.noreply.github.com>
2024-11-22 08:47:48 -0300

    Update player.js

--- player.js
+++ player.js
@@ -140,14 +140,13 @@
             showToast('O video foi invertido.');
         };
 
-   // Toast Mensagem
+      // Toast Mensagem
     function showToast(message, isError = false) {
     const existingToasts = document.querySelectorAll('.toast');
 
     const toast = document.createElement('div');
     toast.className = 'toast';
-
-    // SVG adicionado ao Toast
+	    
     const svgIcon = `
       <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M20.1892 14.0608L19.0592 12.1808C18.8092 11.7708 18.5892 10.9808 18.5892 10.5008V8.63078C18.5892 5.00078 15.6392 2.05078 12.0192 2.05078C8.38923 2.06078 5.43923 5.00078 5.43923 8.63078V10.4908C5.43923 10.9708 5.21923 11.7608 4.97923 12.1708L3.84923 14.0508C3.41923 14.7808 3.31923 15.6108 3.58923 16.3308C3.85923 17.0608 4.46923 17.6408 5.26923 17.9008C6.34923 18.2608 7.43923 18.5208 8.54923 18.7108C8.65923 18.7308 8.76923 18.7408 8.87923 18.7608C9.01923 18.7808 9.16923 18.8008 9.31923 18.8208C9.57923 18.8608 9.83923 18.8908 10.1092 18.9108C10.7392 18.9708 11.3792 19.0008 12.0192 19.0008C12.6492 19.0008 13.2792 18.9708 13.8992 18.9108C14.1292 18.8908 14.3592 18.8708 14.5792 18.8408C14.7592 18.8208 14.9392 18.8008 15.1192 18.7708C15.2292 18.7608 15.3392 18.7408 15.4492 18.7208C16.5692 18.5408 17.6792 18.2608 18.7592 17.9008C19.5292 17.6408 20.1192 17.0608 20.3992 16.3208C20.6792 15.5708 20.5992 14.7508 20.1892 14.0608ZM12.7492 10.0008C12.7492 10.4208 12.4092 10.7608 11.9892 10.7608C11.5692 10.7608 11.2292 10.4208 11.2292 10.0008V6.90078C11.2292 6.48078 11.5692 6.14078 11.9892 6.14078C12.4092 6.14078 12.7492 6.48078 12.7492 6.90078V10.0008Z" fill="#8a5acc"></path>
@@ -155,9 +154,7 @@
       </svg>
     `;
 
-    // Conteúdo do Toast
     toast.innerHTML = `${svgIcon} <span>${message}</span>`;
-
     toast.style.cssText = `
         display: flex;
         align-items: center;
@@ -165,14 +162,14 @@
         position: fixed;
         bottom: ${existingToasts.length > 0 ? (existingToasts.length * 60 + 20) + 'px' : '20px'};
         right: 20px;
-        background-color: ${isError ? '#1d1e2a' : '#14151f'};
+        background-color: ${isError ? '#1d1e2ad9' : '#14151fd9'};
         color: ${isError ? '#FFF' : '#b39ad6'};
         padding: 15px 20px;
         border-radius: 12px;
         font-size: 14px;
         font-family: Arial, sans-serif;
         z-index: 9999;
-        box-shadow: 0 0 15px ${isError ? '#8a5acc' : '#b39ad6'}, 0 0 25px ${isError ? '#b39ad6' : '#8a5acc'};
+        box-shadow: 0 0 15px ${isError ? '#8a5accd9' : '#b39ad6d9'}, 0 0 25px ${isError ? '#b39ad6d9' : '#8a5accd9'};
         transition: opacity 0.5s ease-in-out;
         opacity: 0.9;
         animation: ToastAnim 2s ease 0s 1 normal forwards;
@@ -182,9 +179,10 @@
 
     setTimeout(() => {
         toast.style.opacity = 0;
-        setTimeout(() => toast.remove(), 500);
+        setTimeout(() => toast.remove(), 800);
     }, 2000);
-}
+	}
+
 
         // Controle de velocidade
         const speedButton = document.createElement('button');
