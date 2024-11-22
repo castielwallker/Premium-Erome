5480372985c051d102c4a720b1662f0cdfc784f1
Maad <39955093+castielwallker@users.noreply.github.com>
2024-11-22 08:49:37 -0300

    Update nsfw.js

--- nsfw.js
+++ nsfw.js
@@ -41,34 +41,50 @@
     `;
     document.head.appendChild(style);
 
-    // Variável global para armazenar referência ao Toast
     let toastTimeout;
     let isToastVisible = false;
 
     function showToast(message, isError = false) {
-        if (isToastVisible) return;  // Impedir exibição de múltiplas toasts simultâneas
+    const existingToasts = document.querySelectorAll('.toast');
 
-        isToastVisible = true;  // Marcar Toast como visível
+    const toast = document.createElement('div');
+    toast.className = 'toast';
+	    
+    const svgIcon = `
+      <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20.1892 14.0608L19.0592 12.1808C18.8092 11.7708 18.5892 10.9808 18.5892 10.5008V8.63078C18.5892 5.00078 15.6392 2.05078 12.0192 2.05078C8.38923 2.06078 5.43923 5.00078 5.43923 8.63078V10.4908C5.43923 10.9708 5.21923 11.7608 4.97923 12.1708L3.84923 14.0508C3.41923 14.7808 3.31923 15.6108 3.58923 16.3308C3.85923 17.0608 4.46923 17.6408 5.26923 17.9008C6.34923 18.2608 7.43923 18.5208 8.54923 18.7108C8.65923 18.7308 8.76923 18.7408 8.87923 18.7608C9.01923 18.7808 9.16923 18.8008 9.31923 18.8208C9.57923 18.8608 9.83923 18.8908 10.1092 18.9108C10.7392 18.9708 11.3792 19.0008 12.0192 19.0008C12.6492 19.0008 13.2792 18.9708 13.8992 18.9108C14.1292 18.8908 14.3592 18.8708 14.5792 18.8408C14.7592 18.8208 14.9392 18.8008 15.1192 18.7708C15.2292 18.7608 15.3392 18.7408 15.4492 18.7208C16.5692 18.5408 17.6792 18.2608 18.7592 17.9008C19.5292 17.6408 20.1192 17.0608 20.3992 16.3208C20.6792 15.5708 20.5992 14.7508 20.1892 14.0608ZM12.7492 10.0008C12.7492 10.4208 12.4092 10.7608 11.9892 10.7608C11.5692 10.7608 11.2292 10.4208 11.2292 10.0008V6.90078C11.2292 6.48078 11.5692 6.14078 11.9892 6.14078C12.4092 6.14078 12.7492 6.48078 12.7492 6.90078V10.0008Z" fill="#8a5acc"></path>
+        <path d="M14.8297 20.01C14.4097 21.17 13.2997 22 11.9997 22C11.2097 22 10.4297 21.68 9.87969 21.11C9.55969 20.81 9.31969 20.41 9.17969 20C9.30969 20.02 9.43969 20.03 9.57969 20.05C9.80969 20.08 10.0497 20.11 10.2897 20.13C10.8597 20.18 11.4397 20.21 12.0197 20.21C12.5897 20.21 13.1597 20.18 13.7197 20.13C13.9297 20.11 14.1397 20.1 14.3397 20.07C14.4997 20.05 14.6597 20.03 14.8297 20.01Z" fill="#8a5acc"></path>
+      </svg>
+    `;
 
-        const toast = document.createElement('div');
-        toast.className = 'toast';
-        toast.textContent = message;
+    toast.innerHTML = `${svgIcon} <span>${message}</span>`;
+    toast.style.cssText = `
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        position: fixed;
+        bottom: ${existingToasts.length > 0 ? (existingToasts.length * 60 + 20) + 'px' : '20px'};
+        right: 20px;
+        background-color: ${isError ? '#1d1e2ad9' : '#14151fd9'};
+        color: ${isError ? '#FFF' : '#b39ad6'};
+        padding: 15px 20px;
+        border-radius: 12px;
+        font-size: 14px;
+        font-family: Arial, sans-serif;
+        z-index: 9999;
+        box-shadow: 0 0 15px ${isError ? '#8a5accd9' : '#b39ad6d9'}, 0 0 25px ${isError ? '#b39ad6d9' : '#8a5accd9'};
+        transition: opacity 0.5s ease-in-out;
+        opacity: 0.9;
+        animation: ToastAnim 2s ease 0s 1 normal forwards;
+    `;
 
-        if (isError) {
-            toast.style.backgroundColor = '#101010';
-            toast.style.color = '#ffffff';
-        }
+    document.body.appendChild(toast);
 
-        document.body.appendChild(toast);
-
-        toastTimeout = setTimeout(() => {
-            toast.style.opacity = 0;
-            setTimeout(() => {
-                toast.remove();
-                isToastVisible = false;  // Marcar Toast como não visível
-            }, 500);
-        }, 2000);
-    }
+    setTimeout(() => {
+        toast.style.opacity = 0;
+        setTimeout(() => toast.remove(), 800);
+    }, 2000);
+	}
 
     const addButtonToNavbar = function () {
         const navbarRight = document.querySelector('.navbar-nav.navbar-right');
@@ -87,24 +103,20 @@
                     '.album-thumbnail-container, .media-group img, .media-group video, .vjs-poster'
                 );
 
-                // Verificar se o blur está ativado ou desativado
                 const isBlurred = !albumContainers[0]?.classList.contains('blur');
 
                 albumContainers.forEach(albumContainer => {
                     albumContainer.classList.toggle('blur', isBlurred);
                 });
 
-                // Alterar o estado no localStorage
                 if (isBlurred) {
                     localStorage.setItem('nsfw', 'true');
-                    // Modificar a URL adicionando o prefixo =nsfw
                     if (!window.location.href.includes('=nsfw')) {
                         window.history.replaceState(null, '', window.location.href + '=nsfw');
                     }
                     showToast('O blur foi ativado. Foi detectado ambiente adulto.');
                 } else {
                     localStorage.removeItem('nsfw');
-                    // Remover o prefixo =nsfw da URL
                     const newUrl = window.location.href.replace('=nsfw', '');
                     window.history.replaceState(null, '', newUrl);
                     showToast('O blur foi desativado.');
@@ -114,7 +126,6 @@
             navbarRight.appendChild(nsfwButton);
         }
 
-        // Verificar o estado inicial do blur
         if (localStorage.getItem('nsfw') === 'true') {
             const albumContainers = document.querySelectorAll(
                 '.album-thumbnail-container, .media-group img, .media-group video, .vjs-poster'
